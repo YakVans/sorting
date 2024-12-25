32777c86ae42f99993df4983afd63fe5a39d1478
YakVans <iakovvansovich@gmail.com>
2024-12-25 03:35:36 +0300

    fifth commit

--- main.c
+++ main.c
@@ -14,19 +14,19 @@ int main()
         return 1;
     }
 
-    Queue sorted_nums1;
-    declar_queue(&sorted_nums1);
-    clock_t start1 = clock();
-    select_sort(&sorted_nums1, &nums);
-    clock_t end1 = clock();
-    printf("Время выполнения сортировки выбором: %f\n", (float)(end1 - start1) / CLOCKS_PER_SEC);
-
     Queue sorted_nums2;
     declar_queue(&sorted_nums2);
     clock_t start2 = clock();
     quick_sort(&sorted_nums2, &nums);
     clock_t end2 = clock();
     printf("Время выполнения быстрой сортировки: %f\n", (float)(end2 - start2) / CLOCKS_PER_SEC);
+
+    Queue sorted_nums1;
+    declar_queue(&sorted_nums1);
+    clock_t start1 = clock();
+    select_sort(&sorted_nums1, &nums);
+    clock_t end1 = clock();
+    printf("Время выполнения сортировки выбором: %f\n", (float)(end1 - start1) / CLOCKS_PER_SEC);
 
     char o_filename[256];
     if (create_output(o_filename)) {
